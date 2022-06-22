68608693b44e1525266b272aa972d07e17d96574
Priyajit Bera <priyajit.bera@gmail.com>
2022-06-23 01:37:32 +0530

    unused imports

--- src/main/java/com/projectmicroblog/microblog/service/PostServiceImpl.java
+++ src/main/java/com/projectmicroblog/microblog/service/PostServiceImpl.java
@@ -1,7 +1,5 @@
 package com.projectmicroblog.microblog.service;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
--- src/main/java/com/projectmicroblog/microblog/service/UserServiceImpl.java
+++ src/main/java/com/projectmicroblog/microblog/service/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package com.projectmicroblog.microblog.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
